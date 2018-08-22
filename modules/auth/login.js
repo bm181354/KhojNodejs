f6d5afa5589a75605da6523e3cc7a592c617e89e
biken <bm181354@bu.edu>
2018-08-22 14:35:56 -0400

    minor update on labels

--- modules/auth/login.js
+++ modules/auth/login.js
@@ -8,7 +8,7 @@ module.exports = (router) => {
     "use strict";
       router.post("/api/v1/login", (req, res) => {
       /* your code for invoking validateUserByEmail() and define the callback/closure here*/
-      authController.validateUserByEmail(req.body.userEmail, req.body.password,req.body.secret, (userResponse) => {
+      authController.validateUserByEmail(req.body.email, req.body.password,req.body.secret, (userResponse) => {
 
          //console.log(userResponse)
          if(userResponse.result){
